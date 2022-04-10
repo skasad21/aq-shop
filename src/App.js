69b509fa4a52f9808b36ae6bc554610b24001592
skasad21 <skasadquadir@gmail.com>
2022-04-11 02:38:17 +0600

    broken our homepage into smaller components, finished styling our menu-item

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 // My Component
-import HomePage from "./components/homepage.component";
+import HomePage from "./pages/homepage/homepage.component";
 
 function App() {
   return (
